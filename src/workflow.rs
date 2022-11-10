ca0639bfa2e89a5283c3867870b178ed2bdbc30b
scholegg <gildeveloper0@gmail.com>
2022-11-10 19:58:41 -0400

    chore: add workflow

---
+++ src/workflow.rs
@@ -0,0 +1,10 @@
+pub fn workflow() {
+    let number1 = 24;
+    let number2 = 42;
+    if number1 > number2 {
+        println!("{} > {}", number1, number2);
+    }
+    else {
+        println!("{} < {}", number1, number2);
+    }
+}
