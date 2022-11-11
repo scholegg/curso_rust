09b55844fcde8e9ddd38aad4f494ccf5522d75ad
scholegg <gildeveloper0@gmail.com>
2022-11-10 20:38:19 -0400

    chore: include workflow in lib file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use curso_rust::name;
+// use curso_rust::name;
+use curso_rust::workflow;
 fn main() {
-    curso_rust::name::name();
+    // curso_rust::name::name();
+    curso_rust::workflow::workflow();
 }
