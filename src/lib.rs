77d1da32a1c751b9840bb73502f2c55d2af84eff
scholegg <gildeveloper0@gmail.com>
2022-11-10 20:59:30 -0400

    feat: include while_loop in lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod name;
 pub mod workflow;
 pub mod working_data_input;
+pub mod while_loop;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // use curso_rust::name;
 // use curso_rust::workflow;
-use curso_rust::working_data_input;
+// use curso_rust::working_data_input;
+use curso_rust::while_loop;
 fn main() {
-    // curso_rust::name::name();
-    // curso_rust::workflow::workflow();
-    curso_rust::working_data_input::working_data_input();
+  // curso_rust::name::name();
+  // curso_rust::workflow::workflow();
+  // curso_rust::working_data_input::working_data_input();
+	curso_rust::while_loop::while_loop();
 }
