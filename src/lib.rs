0e535c9616b32c9db102fc07783b24be7f64bc11
scholegg <gildeveloper0@gmail.com>
2022-11-10 20:45:51 -0400

    chore: include workflow in lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod name;
 pub mod workflow;
+pub mod working_data_input;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use curso_rust::name;
-use curso_rust::workflow;
+// use curso_rust::workflow;
+use curso_rust::working_data_input;
 fn main() {
     // curso_rust::name::name();
-    curso_rust::workflow::workflow();
+    // curso_rust::workflow::workflow();
+    curso_rust::working_data_input::working_data_input();
 }
